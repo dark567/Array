d6fe2021131420f1dad1e4dd8721be9952d4fa1e
dark567 <darkmit@list.ru>
2019-08-29 20:13:25 +0300

    '201908292013'

---
+++ sergeyM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRelease2
+{
+    class sergeyM
+    {
+        public void Show()
+        {
+            Console.WriteLine("Hi");
+        }
+
+    }
+}
